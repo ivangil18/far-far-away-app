c401ed51ad9f6fb20da9e7524ce3053f65e12933
Ivan Gil <igil.dev@gmail.com>
2023-07-19 00:28:30 -0400

    app structured, initial styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return (
+    <div className="logo">
+      <img src="./palm.png" alt="palm" />
+      <h1>FAR FAR AWAY</h1>
+      <img src="./suitecase.png" alt="luggage" />
+    </div>
+  );
+}
+function Form() {
+  function handleAdd() {}
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+      <input type="dropdown" placeholder="1" className="quantity" />
+      <input type="text" placeholder="item..." />
+      <button onClick={handleAdd}>ADD</button>
+    </div>
+  );
+}
+function PackingList() {
+  return (
+    <main className="list">
+      <div className="listItems"></div>
+      <div className="buttons">
+        <button>Sort By Input Order</button>
+        <button>Clear List</button>
+      </div>
+    </main>
+  );
+}
+function Item({ item }) {
+  console.log({ item });
+  return (
+    <>
+      <span className="item">
+        <input type="checkbox" />
+        {item.quantity} {item.name}
+        <button className="close">&times;</button>
+      </span>
+    </>
+  );
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>
+        🧳 You have qty items on your list, and you already have packed 0 (0%)
+      </em>
+    </footer>
+  );
+}
